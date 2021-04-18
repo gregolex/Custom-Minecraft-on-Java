aae30e4c71000ad5402ddde17ef374d50391a50e
Gregory Alex Kalaydjian <gregorykalay@gmail.com>
2021-04-17 17:28:17 -0700

    Update vector3Float.java
    Updated package requirements and syntax

--- vector3Float.java
+++ vector3Float.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hw3cs4450;
+//include package
 
 /**
- *
  * Purpose Holds the x y z values in 3d vector space
  */
 public class vector3Float 
@@ -18,5 +12,4 @@ public class vector3Float
         this.y = y;
         this.z = z;
     }
-    
 }
