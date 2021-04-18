2081c9d237326dfef157dfa25260a7b8dabfdfa6
Gregory Alex Kalaydjian <gregorykalay@gmail.com>
2021-04-17 17:26:02 -0700

    Update PlayMusic.java

--- PlayMusic.java
+++ PlayMusic.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hw3cs4450;
+//include package
+
 import java.io.File; 
 import java.io.IOException; 
 import java.util.Scanner; 
